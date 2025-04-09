ae4319708ce1ad09be29f0a06b96e3465a8004e4
Dylan Green <cobaltgoldcsprinks@gmail.com>
2025-04-09 15:11:34 -0600

    Update Sidebar display style

--- client/src/components/LoadingElement.tsx
+++ client/src/components/LoadingElement.tsx
@@ -1,6 +1,5 @@
-import ring from "../assets/ring.gif"
 export const LoadingElement = () => {
     return (
-        <img id="loading" src={ring} alt="Loading Indicator" />
+        <span className="loader"></span>
     )
 }
--- client/src/components/MapSidebar.tsx
+++ client/src/components/MapSidebar.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { LoadingElement } from "./LoadingElement";
 
 export enum MapViewType {
     NONE = 0,
@@ -12,26 +13,41 @@ export enum MapViewType {
 export interface SidebarProps {
     selectedMode: MapViewType;
     setSelectedMode: React.Dispatch<React.SetStateAction<MapViewType>>;
+    loading: boolean;
 }
 
 
 
-export const MapSidebar = ({selectedMode, setSelectedMode} : SidebarProps) => {
+export const MapSidebar = ({selectedMode, setSelectedMode, loading} : SidebarProps) => {
 
+    const [sidebarHidden, setSidebarHidden] = useState<boolean>(false);
     return (
-        <div id="map-sidebar">
-            <SelectModeButton text={"Truck Stops"}
-                              disabled={selectedMode === MapViewType.STOPS}
-                              onclick={() => setSelectedMode(MapViewType.STOPS)} />
-            <SelectModeButton text={"Freight Routes into Utah"}
-                              disabled={selectedMode === MapViewType.INTO_UTAH}
-                              onclick={() => setSelectedMode(MapViewType.INTO_UTAH)} />
-            <SelectModeButton text={"Freight Routes from Utah"}
-                              disabled={selectedMode === MapViewType.OUT_OF_UTAH}
-                              onclick={() => setSelectedMode(MapViewType.OUT_OF_UTAH)} />
-            <SelectModeButton text={"Heatmap"}
-                              disabled={selectedMode === MapViewType.HEAT}
-                              onclick={() => setSelectedMode(MapViewType.HEAT)} />
+        <div className="sidebar-container" data-hidden={sidebarHidden}>
+            <div id="hidebar">
+                {loading ? <LoadingElement/> : <span/>}
+                <span id="sidebar-title">Datasets</span>
+                <button type="button"
+                        id="hide-btn"
+                        onClick={() => setSidebarHidden(!sidebarHidden)}>
+                    {sidebarHidden ? "<" : ">"}
+                </button>
+            </div>
+            {!sidebarHidden && (
+                <div id="map-sidebar">
+                <SelectModeButton text={"Truck Stops"}
+                                disabled={selectedMode === MapViewType.STOPS}
+                                onclick={() => setSelectedMode(MapViewType.STOPS)} />
+                <SelectModeButton text={"Freight Routes into Utah"}
+                                disabled={selectedMode === MapViewType.INTO_UTAH}
+                                onclick={() => setSelectedMode(MapViewType.INTO_UTAH)} />
+                <SelectModeButton text={"Freight Routes from Utah"}
+                                disabled={selectedMode === MapViewType.OUT_OF_UTAH}
+                                onclick={() => setSelectedMode(MapViewType.OUT_OF_UTAH)} />
+                <SelectModeButton text={"Heatmap"}
+                                disabled={selectedMode === MapViewType.HEAT}
+                                onclick={() => setSelectedMode(MapViewType.HEAT)} />
+                </div>
+            )}
         </div>
     )
 }
